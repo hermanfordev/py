f2dcb47a7b32006c871a3dec0ca83180380dbbe1
hermanfordev <hermanfordev@gmail.com>
2018-08-19 16:12:53 +0900

    syntax folder added

---
+++ syntax/List.py
@@ -0,0 +1,15 @@
+#list
+
+numBox = [1, 2, 3, 4, 5]
+print(numBox)
+
+abox = [1, 'two', 3, 'four']
+
+print(abox[1])
+
+len(abox)
+
+del abox[1]
+
+
+
---
+++ syntax/String.py
@@ -0,0 +1,18 @@
+print('Hello world')
+print("Hello World")
+
+print("Hllo" "'" "world")
+print("Hello ' my world")
+
+#escape
+print("Hello \" world")
+
+#new line
+print("H\nE\nL\nL\nO\nW\nO\nR\nL\nD")
+
+#docstring
+print('''
+1
+2
+3
+''')
